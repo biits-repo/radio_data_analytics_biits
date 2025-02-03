76cae4cce66762b01cb2603c2e742d9acdf02e1b
MohammadAdeebKhan <147697159+MohammadAdeebKhan@users.noreply.github.com>
2025-02-03 13:40:14 +0530

    Update WhisperAgent.py

--- WhisperAgent.py
+++ WhisperAgent.py
@@ -1,70 +1,52 @@
-import whisper
-from pathlib import Path
-import subprocess
+from openai import OpenAI
+from dotenv import load_dotenv
 import os
+from pathlib import Path
+from groq import Groq
+import whisper
+from Sponsors import GetSponsors
 
 
-class TranscribingAgent:
+load_dotenv()
 
-    def __init__(self  , audio):
-        
+
+class WhisperAgent:
+
+    def __init__(self):
+
+        # Open AI client initialization
+        #self.api_key = os.getenv("GROQ_KEY")
+        #self.client = Groq(api_key=self.api_key)
+
         self.model = whisper.load_model('large')
-        self.audio_file = audio
-
-
-        self.audio_dir = Path("Chunks")
-
-        self.audio_dir.mkdir(exist_ok=True)
+        # self.folder_path = audio_path
+        
         
 
-    def chunk_audio(self):
-
-        chunk_path = self.audio_dir / 'chunk_%d.mp3'
+    def transcribe(self,chunk_dir):
         
-        command = [
-            'ffmpeg',
-            '-i' , self.audio_file,
-            '-f' , 'segment',
-            '-segment_time' , str(600),
-            '-c' , 'copy' ,
-            '-c:a' , 'libmp3lame',
-            '-b:a' , '128k',
-            '-reset_timestamps' , '1',
-            chunk_path
-        ]
+        super_list = []
 
         try:
-            chunk_path = subprocess.run(command , check = True , stderr = subprocess.PIPE)
-            return True
+            for i in os.listdir(chunk_dir):
+                chunk_path = os.path.join(chunk_dir,i)
+                transcription = self.model.transcribe(chunk_path)
+                segments = transcription['segments']
 
-        except subprocess.CalledProcessError as e:
-            print(f"Eroor occurred while running ffmpeg: {e.stderr.decode()}")
-            return False
-        
-
-    def transcribe_audio(self):
-
-        chunker = self.chunk_audio()
-        full_transcription = ""
-        if chunker:
-
-            audio_dir = Path("Chunks")
-
-            for i in audio_dir.glob('chunk_*.mp3'):
-
-                result = self.model.transcribe(str(i) )
-
-                full_transcription+=result['text']
-
-            print(full_transcription)
+                
+                for segment in segments:
+                   super_list.append((f"{segment['start']} - {segment['end']}", segment['text']))
             
-            for i in self.audio_dir.glob("chunk_*.mp3"):
-                os.unlink(i)
-        else:
-            print("Error occurred while making chunks")
 
+            
+        except Exception as error:
+            print(str(error))
+        
+        for i in os.listdir(chunk_dir):
+            chunk_path = os.path.join(chunk_dir,i)
+            os.unlink(chunk_path)
+        
+        print("Cleared all chunks..............")
 
-input_dir = Path("InputAudios")
-audio_file_path = input_dir / "C:\\Users\\mohammad.adeeb\\Downloads\\InternetDownloads\\audio2.mp3"
-model = TranscribingAgent(audio_file_path)
-model.transcribe_audio()
+        return super_list
+                
