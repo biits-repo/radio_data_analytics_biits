a3e1f2d6d9f9ff0b5e57f5fb739ceb1ecaa3b61b
MohammadAdeebKhan <147697159+MohammadAdeebKhan@users.noreply.github.com>
2025-02-24 11:42:03 +0530

    Update WhisperAgent.py

--- WhisperAgent.py
+++ WhisperAgent.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from groq import Groq
 import whisper
 from Sponsors import GetSponsors
+from datetime import datetime
+
+
 
 
 load_dotenv()
@@ -19,23 +22,52 @@ class WhisperAgent:
         #self.client = Groq(api_key=self.api_key)
 
         self.model = whisper.load_model('large')
-        # self.folder_path = audio_path
-        
-        
+    
 
-    def transcribe(self,chunk_dir):
+
+
+    def transcribe(self,chunk_dir,file_name,audio_id):
         
-        super_list = []
+        self.super_list = []
+
+        self.audio_id = audio_id
+        
 
         try:
             for i in os.listdir(chunk_dir):
+
+                self.chunk_details = {}
+                self.chunk_time_stamps = ""
+                self.chunk_text = ""
+
                 chunk_path = os.path.join(chunk_dir,i)
                 transcription = self.model.transcribe(chunk_path)
                 segments = transcription['segments']
 
+                self.chunk_creation_date = datetime.now().date()
+                self.chunk_creation_date_time = datetime.now()
+
                 
                 for segment in segments:
-                   super_list.append((f"{segment['start']} - {segment['end']}", segment['text']))
+                   
+                   self.chunk_text+= f"{segment['text']} $!"
+                   self.chunk_time_stamps+= f" {segment['start']} - {segment['end']} $!"
+                
+                self.chunk_details['chunk_file_name'] = f"{file_name} / {i}"
+                self.chunk_details['chunk_text'] = self.chunk_text
+                self.chunk_details['timestamps'] = self.chunk_time_stamps   
+                self.chunk_details['audio_id'] = self.audio_id
+                self.chunk_details['chunk_creation_date'] = self.chunk_creation_date
+                self.chunk_details['chunk_creation_date_time'] = self.chunk_creation_date_time
+            
+                self.super_list.append(self.chunk_details)
+
+            
+            for i in self.super_list:
+                print(i)
+                print('\n'*5)
+
+                   #super_list.append((f"{segment['start']} - {segment['end']}", segment['text']))
             
 
             
@@ -48,5 +80,5 @@ class WhisperAgent:
         
         print("Cleared all chunks..............")
 
-        return super_list
+        return self.super_list
                 
