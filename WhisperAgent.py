aa045f28722d9604e6f38d1f82a913683bc94fdc
MohammadAdeebKhan <147697159+MohammadAdeebKhan@users.noreply.github.com>
2025-02-25 18:13:17 +0530

    Update WhisperAgent.py

--- WhisperAgent.py
+++ WhisperAgent.py
@@ -32,35 +32,74 @@ class WhisperAgent:
 
         self.audio_id = audio_id
         
+        previous_end_time = 0  # Initialize to 0 to start the first chunk at 0
 
         try:
             for i in os.listdir(chunk_dir):
-
                 self.chunk_details = {}
                 self.chunk_time_stamps = ""
                 self.chunk_text = ""
 
-                chunk_path = os.path.join(chunk_dir,i)
+                chunk_path = os.path.join(chunk_dir, i)
                 transcription = self.model.transcribe(chunk_path)
                 segments = transcription['segments']
 
                 self.chunk_creation_date = datetime.now().date()
                 self.chunk_creation_date_time = datetime.now()
 
-                
                 for segment in segments:
-                   
-                   self.chunk_text+= f"{segment['text']} $!"
-                   self.chunk_time_stamps+= f" {segment['start']} - {segment['end']} $!"
-                
+                    # Adjust the start and end time based on the previous chunk's end time
+                    start_time = segment['start'] + previous_end_time
+                    end_time = segment['end'] + previous_end_time
+
+                    # Update the timestamps string to reflect adjusted times
+                    self.chunk_time_stamps += f" {start_time} - {end_time} $!"
+
+                    # Append the segment text to the chunk's text
+                    self.chunk_text += f"{segment['text']} $!"
+
+                # Save chunk details
                 self.chunk_details['chunk_file_name'] = f"{file_name} / {i}"
                 self.chunk_details['chunk_text'] = self.chunk_text
-                self.chunk_details['timestamps'] = self.chunk_time_stamps   
+                self.chunk_details['timestamps'] = self.chunk_time_stamps
                 self.chunk_details['audio_id'] = self.audio_id
                 self.chunk_details['chunk_creation_date'] = self.chunk_creation_date
                 self.chunk_details['chunk_creation_date_time'] = self.chunk_creation_date_time
-            
+
+                # Add to the super list
                 self.super_list.append(self.chunk_details)
+
+                # Update the previous_end_time for the next chunk
+                previous_end_time = segments[-1]['end']
+
+        # try:
+        #     for i in os.listdir(chunk_dir):
+
+        #         self.chunk_details = {}
+        #         self.chunk_time_stamps = ""
+        #         self.chunk_text = ""
+
+        #         chunk_path = os.path.join(chunk_dir,i)
+        #         transcription = self.model.transcribe(chunk_path)
+        #         segments = transcription['segments']
+
+        #         self.chunk_creation_date = datetime.now().date()
+        #         self.chunk_creation_date_time = datetime.now()
+
+                
+        #         for segment in segments:
+                   
+        #            self.chunk_text+= f"{segment['text']} $!"
+        #            self.chunk_time_stamps+= f" {segment['start']} - {segment['end']} $!"
+                
+        #         self.chunk_details['chunk_file_name'] = f"{file_name} / {i}"
+        #         self.chunk_details['chunk_text'] = self.chunk_text
+        #         self.chunk_details['timestamps'] = self.chunk_time_stamps   
+        #         self.chunk_details['audio_id'] = self.audio_id
+        #         self.chunk_details['chunk_creation_date'] = self.chunk_creation_date
+        #         self.chunk_details['chunk_creation_date_time'] = self.chunk_creation_date_time
+            
+        #         self.super_list.append(self.chunk_details)
 
             
             for i in self.super_list:
