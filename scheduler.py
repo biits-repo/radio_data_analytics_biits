f560304fa4804a3785113456b062288560f49f41
MohammadAdeebKhan <147697159+MohammadAdeebKhan@users.noreply.github.com>
2025-03-21 22:34:23 +0530

    Add files via upload

---
+++ scheduler.py
@@ -0,0 +1,22 @@
+import schedule
+import time
+from GetAudioPaths import main
+import logging
+
+logging.basicConfig()
+schedule_logger = logging.getLogger('schedule')
+schedule_logger.setLevel(level=logging.DEBUG)
+
+
+schedule.every().day.at("18:21").do(main,"C:\\Users\\mohammad.adeeb\\OpenAIProject\\audio_path.csv","C:\\Folders")
+
+
+
+
+
+
+
+while True:
+    schedule.run_pending()
+    # schedule.run_all()
+    # schedule.clear()
